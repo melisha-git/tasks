8bd7a5f770be5ab1805f78dde27fdce1af31bc2b
melisha-git <egorka_savelev_2001@rambler.ru>
2021-06-10 18:25:17 +0300

    a_plus_b

---
+++ a_plus_b.cpp
@@ -0,0 +1,33 @@
+/*Требуется сложить два целых числа А и В.
+ * Входные данные
+ * В единственной строке входного файла INPUT.TXT записаны два натуральных числа через пробел. Значения чисел не превышают 109.
+ *
+ * Выходные данные
+ * В единственную строку выходного файла OUTPUT.TXT нужно вывести одно целое число — сумму чисел А и В.
+*/
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int displayError(std::string err) {
+	std::cerr << err << std::endl;
+	return 0;
+}
+
+int main() {
+	std::ifstream ifs("INPUT.TXT");
+	std::ofstream ofs("OUTPUT.TXT");
+	if (!ofs.is_open() || !ifs.is_open())
+		return displayError("Error files");
+	int a, b;
+	std::string str;
+	std::getline(ifs, str);
+	a = std::stoi(str);
+	b = std::stoi(str.erase(0, str.find(' ') + 1));
+	ofs << a + b;
+	ifs.close();
+	ofs.close();
+	return 0;
+}
